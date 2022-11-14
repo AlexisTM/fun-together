3eb08b72e3018cc2d4d8910bd6ec7d791a805f6b
Alexis Paques <alexis.paques@gmail.com>
2022-11-14 19:23:08 +0100

    Fix deadlock

--- server/src/game.rs
+++ server/src/game.rs
@@ -3,7 +3,7 @@ use crate::comm::{Command, GameRequest, GameState};
 
 use std::collections::HashMap;
 use std::net::TcpStream;
-use std::sync::{RwLock};
+use std::sync::RwLock;
 
 use tungstenite::protocol::frame::coding::CloseCode;
 use tungstenite::WebSocket;
@@ -18,7 +18,29 @@ pub struct Game {
 }
 
 impl Game {
-    pub fn new(host: Actor) -> Self {
+    pub fn new(mut host: Actor) -> Self {
+        host.send_request(&GameRequest::default());
+        host.send_request(&Command::State {
+            players: vec![1, 2, 3],
+            state: GameState::Preparing,
+        });
+        host.send_request(&Command::Prepare {
+            min_players: 2,
+            max_players: 8,
+        });
+        host.send_request(&Command::Stop());
+        host.send_request(&Command::PrepareReply {
+            key: "hey".to_string(),
+        });
+        host.send_request(&Command::From {
+            from: 1,
+            data: "".to_string(),
+        });
+        host.send_request(&Command::To {
+            to: vec![1, 2],
+            data: "".to_string(),
+        });
+
         Self {
             last_id_given: 0,
             state: GameState::Preparing,
@@ -29,12 +51,9 @@ impl Game {
         }
     }
 
-    pub fn get_host(&mut self) -> &mut RwLock<Actor> {
-        &mut self.host
-    }
-
     pub fn add(&mut self, player_ws: WebSocket<TcpStream>) -> bool {
         let mut players = self.players.write().unwrap();
+
         self.last_id_given += 1;
         let mut player = Actor::new(self.last_id_given, player_ws);
         if players.len() < self.max_players
@@ -45,17 +64,8 @@ impl Game {
         }
         player.disconnect(CloseCode::Error);
 
-        let mut host = self.host.write().unwrap();
-        host.send_request(&self.game_state_command());
+        // host.send_request(&self.game_state_command());
         false
-    }
-
-    fn game_state_command(&self) -> Command {
-        let players = self.players.read().unwrap();
-        return Command::State {
-            players: players.keys().cloned().collect(),
-            state: self.state.clone(),
-        };
     }
 
     pub fn update(&mut self) -> bool {
@@ -65,29 +75,7 @@ impl Game {
         let curr_state = self.state.clone();
 
         match self.state {
-            GameState::Preparing => {
-                host.send_request(&GameRequest::default());
-                host.send_request(&Command::State {
-                    players: vec![1, 2, 3],
-                    state: self.state.clone(),
-                });
-                host.send_request(&Command::Prepare {
-                    min_players: 2,
-                    max_players: 8,
-                });
-                host.send_request(&Command::Stop());
-                host.send_request(&Command::PrepareReply {
-                    key: "hey".to_string(),
-                });
-                host.send_request(&Command::From {
-                    from: 1,
-                    data: "".to_string(),
-                });
-                host.send_request(&Command::To {
-                    to: vec![1, 2],
-                    data: "".to_string(),
-                });
-            } // Resettting the game data, & accepting
+            GameState::Preparing => {} // Resettting the game data, & accepting
             GameState::Lobby => {
                 let enough_players =
                     players.len() >= self.min_players && players.len() <= self.max_players;
@@ -129,6 +117,7 @@ impl Game {
         });
 
         while let Some(host_cmd) = host.read_command() {
+            println!("{:?}", host_cmd);
             match host_cmd {
                 Command::Prepare {
                     min_players,
@@ -154,8 +143,11 @@ impl Game {
                 }
                 Command::Kick { player } => {
                     players.remove_entry(&player);
-                    let mut host = self.host.write().unwrap();
-                    host.send_request(&self.game_state_command());
+                    let msg = Command::State {
+                        players: players.keys().cloned().collect(),
+                        state: self.state.clone(),
+                    };
+                    host.send_request(&msg);
                 }
                 Command::Stop() => {
                     self.state = GameState::Stopping;
@@ -195,7 +187,11 @@ impl Game {
         }
 
         if curr_state != self.state {
-            host.send_request(&self.game_state_command());
+            let msg = Command::State {
+                players: players.keys().cloned().collect(),
+                state: self.state.clone(),
+            };
+            host.send_request(&msg);
             println!("{:?}", self.state);
         }
         true
